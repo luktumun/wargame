8498c64907ea7b859c46f523fae0fe009611c6cd
rajendra <30436194+luktumun@users.noreply.github.com>
2024-04-22 14:44:29 +0530

    Update setupRemoteMongo.sh

--- setupRemoteMongo.sh
+++ setupRemoteMongo.sh
@@ -1,2 +1,2 @@
 # Setup file template to upload data to MongoDB Atlas
-mongoimport --uri "mongodb://ac-mpp1kok-shard-00-00.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-01.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-02.phvwerh.mongodb.net:27017/wargame?replicaSet=atlas-yrs1tu-shard-0" --ssl --authenticationDatabase admin --username srajendra923 --password t3blH7zgWe557PwM  --drop --collection videos --file data/VideoData.json
+mongoimport --uri "mongodb://ac-mpp1kok-shard-00-00.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-01.phvwerh.mongodb.net:27017,ac-mpp1kok-shard-00-02.phvwerh.mongodb.net:27017/wargame?replicaSet=atlas-yrs1tu-shard-0" --ssl --authenticationDatabase admin --username srajendra923 --password X3fmgJKFv4GdQYpw  --drop --collection videos --file data/VideoData.json
